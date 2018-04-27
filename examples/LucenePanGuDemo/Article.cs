bd3794fe83f5cf61745c35be14794740aaf1d017
virtual <1185513330@qq.com>
2018-04-27 14:54:08 +0800

    增加分词Demo Signed-off-by: virtual <1185513330@qq.com>

---
+++ examples/LucenePanGuDemo/Article.cs
@@ -0,0 +1,15 @@
+﻿namespace LucenePanGuDemo
+{
+    /// <summary>文章实体
+    /// </summary>
+    public class Article
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Author { get; set; }
+    }
+}
